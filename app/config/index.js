e21d019853e9a1a0a56d14779fe9fbf8146ad70a
steve-dickinson <steve.dickinson@defra.gov.uk>
2021-07-01 10:15:46 +0100

    Add env to config

--- app/config/index.js
+++ app/config/index.js
@@ -3,6 +3,7 @@ module.exports = {
   port: process.env.PORT || 3000,
   env: process.env.NODE_ENV || 'development',
   serviceName: 'SFI Api Gateway for connecting to Crown Hosting',
+  chApi: process.env.CH_API,
   chApiId: process.env.CH_API_ID,
   chApiKey: process.env.CH_API_KEY
 }
