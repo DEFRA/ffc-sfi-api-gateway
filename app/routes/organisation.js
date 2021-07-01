f6db3be60fa872cd6cb7d1e31c89c08a89c4de44
steve-dickinson <steve.dickinson@defra.gov.uk>
2021-07-01 12:02:03 +0100

    Test another simple route

--- app/routes/organisation.js
+++ app/routes/organisation.js
@@ -2,40 +2,61 @@ const wreck = require('@hapi/wreck')
 const joi = require('joi')
 const { chApi, chApiId, chApiKey } = require('../config')
 
-module.exports = {
-  method: 'GET',
-  path: '/organisation/search/sbi/{sbi}',
-  options: {
-    description: 'Get organisation details',
-    notes: 'Returns organisation details by the sbi passed in the path',
-    tags: ['api'],
-    validate: {
-      params: joi.object({
-        sbi: joi.string().min(9).max(9)
+const responseModel = joi.array().items(joi.object({
+  id: joi.string(),
+  name: joi.string(),
+  sbi: joi.number(),
+  additionalSbiIds: joi.array().items(joi.number()),
+  confirmed: joi.boolean(),
+  lastUpdatedOn: joi.string()
+})).label('Result')
+
+module.exports = [
+  {
+    method: 'GET',
+    path: '/organisation/{sbi}',
+    handler: (request, h) => {
+      return h.proxy({
+        uri: `${chApi}organisation/search/sbi/?sbi=${request.params.sbi}`
       })
     }
   },
-  handler: (request, h) => {
-    return h.proxy({
-      mapUri: (req) => {
-        const sbi = request.params.sbi ? request.params.sbi : ''
-        return {
-          uri: `${chApi}organisation/search/sbi/?sbi=${sbi}`,
-          headers: {
-            'api-id': chApiId,
-            'api-key': chApiKey
-          },
-          passThrough: true,
-          xforward: true
-        }
+  {
+    method: 'GET',
+    path: '/organisation/search/sbi/{sbi}',
+    options: {
+      description: 'Get organisation details',
+      notes: 'Returns organisation details by the sbi passed in the path',
+      tags: ['api'],
+      validate: {
+        params: joi.object({
+          sbi: joi.string().length(9)
+        })
       },
-      onResponse: async (err, res) => {
-        console.log(err)
-        const payload = await wreck.read(res, { json: true })
-        const response = h.response(payload)
-        response.headers = res.headers
-        return response
-      }
-    })
+      response: { schema: responseModel }
+    },
+    handler: (request, h) => {
+      return h.proxy({
+        mapUri: (req) => {
+          const sbi = request.params.sbi ? request.params.sbi : ''
+          return {
+            uri: `${chApi}organisation/search/sbi/?sbi=${sbi}`,
+            headers: {
+              'api-id': chApiId,
+              'api-key': chApiKey
+            },
+            passThrough: true,
+            xforward: true
+          }
+        },
+        onResponse: async (err, res) => {
+          console.log(err)
+          const payload = await wreck.read(res, { json: true })
+          const response = h.response(payload)
+          response.headers = res.headers
+          return response
+        }
+      })
+    }
   }
-}
+]
