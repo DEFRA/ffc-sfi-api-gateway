bfd4dda288c5c017820b9d2bfa9370235f1f5e6a
steve-dickinson <steve.dickinson@defra.gov.uk>
2021-07-02 11:49:19 +0100

    Update port, Start scripts, removed unused files

--- app/config/index.js
+++ app/config/index.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 3005,
   env: process.env.NODE_ENV || 'development',
   serviceName: 'SFI Api Gateway for connecting to Crown Hosting',
   chApi: process.env.CH_API,
