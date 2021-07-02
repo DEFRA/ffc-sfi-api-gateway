950129d03a15aa4ae336a2dd4ccd5cd817ac4dbe
Steve Dickinson <32902665+steve-dickinson@users.noreply.github.com>
2021-07-02 14:22:16 +0100

    Create an API Gateway for Crown Hosting
    https://eaflood.atlassian.net/browse/SFI-585
    
    * Add proxy organisation test
    
    * add hapi-swagger
    
    * add validation
    
    * Add hapi-swagger with .synk policy
    
    * add synk and swagger

--- app/server.js
+++ app/server.js
@@ -1,14 +1,47 @@
-const Hapi = require('@hapi/hapi')
+const hapi = require('@hapi/hapi')
+const H2o2 = require('@hapi/h2o2')
+const config = require('./config')
 
-const server = Hapi.server({
-  port: process.env.PORT
-})
+async function createServer () {
+  // Create the hapi server
+  const server = hapi.server({
+    port: config.port,
+    routes: {
+      validate: {
+        options: {
+          abortEarly: false
+        }
+      }
+    },
+    router: {
+      stripTrailingSlash: true
+    }
+  })
 
-const routes = [].concat(
-  require('./routes/healthy'),
-  require('./routes/healthz')
-)
+  const swaggerOptions = {
+    info: {
+      title: 'SFI Crown Hosting API Gateway Documentation',
+      version: '1.0.1'
+    }
+  }
 
-server.route(routes)
+  await server.register([
+    require('@hapi/inert'),
+    require('@hapi/vision'),
+    {
+      plugin: require('hapi-swagger'),
+      options: swaggerOptions
+    }
+  ])
 
-module.exports = server
+  // Register the plugins
+  await server.register(require('@hapi/inert'))
+  await server.register(H2o2)
+  await server.register(require('./plugins/router'))
+  await server.register(require('blipp'))
+  await server.register(require('./plugins/logging'))
+
+  return server
+}
+
+module.exports = createServer
