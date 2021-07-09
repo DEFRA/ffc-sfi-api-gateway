efbb159448a1edddc2b9bb95af19f8f5c337d898
steve-dickinson <steve.dickinson@defra.gov.uk>
2021-07-09 08:17:46 +0100

    Update to allow path pass through for GET and POST

--- app/plugins/router.js
+++ app/plugins/router.js
@@ -1,4 +1,5 @@
 const routes = [].concat(
+  require('../routes'),
   require('../routes/healthy'),
   require('../routes/healthz'),
   require('../routes/organisation')
---
+++ app/routes/index.js
@@ -0,0 +1,51 @@
+const wreck = require('@hapi/wreck')
+const { chApi, chApiId, chApiKey } = require('../config')
+
+module.exports = [{
+  method: 'GET',
+  path: '/{path*}',
+  options: {
+    description: 'Get calls on Crown Hosting endpoints',
+    notes: 'Returns data from Crown Hosting endpoint',
+    tags: ['api']
+  },
+  handler: (request, h) => {
+    return h.proxy(proxyCall(request, h))
+  }
+},
+{
+  method: 'POST',
+  path: '/{path*}',
+  options: {
+    description: 'Post calls on Crown Hosting endpoints',
+    notes: 'Returns data from Crown Hosting endpoint',
+    tags: ['api'],
+    payload: { parse: false }
+  },
+  handler: (request, h) => {
+    return h.proxy(proxyCall(request, h))
+  }
+}]
+
+const proxyCall = (request, h) => {
+  return {
+    mapUri: (req) => {
+      return {
+        uri: `${chApi.replace(',', '')}${request.path}`,
+        headers: {
+          'api-id': chApiId,
+          'api-key': chApiKey
+        },
+        passThrough: true,
+        xforward: true
+      }
+    },
+    onResponse: async (err, res) => {
+      console.log(err)
+      const payload = await wreck.read(res, { json: true })
+      const response = h.response(payload)
+      response.headers = res.headers
+      return response
+    }
+  }
+}
