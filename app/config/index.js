bb8f18c013e24becee65c4e8c6d522df229b4eb1
David Stone <davidjamesstone@gmail.com>
2021-07-15 15:37:21 +0100

    Add default value for USE_MOCK

--- app/config/index.js
+++ app/config/index.js
@@ -3,7 +3,7 @@ module.exports = {
   port: process.env.PORT || 3005,
   env: process.env.NODE_ENV || 'development',
   serviceName: 'SFI Api Gateway for connecting to Crown Hosting',
-  useMock: process.env.USE_MOCK || false,
+  useMock: process.env.USE_MOCK === 'true',
   chApi: process.env.CH_API,
   chApiId: process.env.CH_API_ID,
   chApiKey: process.env.CH_API_KEY
