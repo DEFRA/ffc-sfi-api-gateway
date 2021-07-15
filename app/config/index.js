012b6a2c327b88dde436e0b46a88a29596ed143c
David Stone <davidjamesstone@gmail.com>
2021-07-15 13:12:19 +0100

    Add mock router

--- app/config/index.js
+++ app/config/index.js
@@ -3,6 +3,7 @@ module.exports = {
   port: process.env.PORT || 3005,
   env: process.env.NODE_ENV || 'development',
   serviceName: 'SFI Api Gateway for connecting to Crown Hosting',
+  useMock: process.env.USE_MOCK || false,
   chApi: process.env.CH_API,
   chApiId: process.env.CH_API_ID,
   chApiKey: process.env.CH_API_KEY
