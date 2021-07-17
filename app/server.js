6c6aad096b27d2d40dd505296b3f6fc54695079d
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2021-07-17 17:05:34 +0100

    Test header passthrough

--- app/server.js
+++ app/server.js
@@ -40,7 +40,9 @@ async function createServer () {
 
   // Register the plugins
   await server.register(require('@hapi/inert'))
-  await server.register(H2o2)
+  await server.register(H2o2, {
+    passThrough: true
+  })
   await server.register(router)
   await server.register(require('blipp'))
   // await server.register(require('./plugins/logging'))
