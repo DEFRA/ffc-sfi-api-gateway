f810dd2750edc541d59ee603cbae65e7fd6ce3b6
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2021-07-19 07:31:41 +0100

    Inject callerId rather than use passthrough

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,47 +5,50 @@ const proxyCall = () => {
   return {
     mapUri: (req) => {
       const query = req.url.search ? req.url.search : ''
+      const callerId = req.headers.callerid ? { callerid: req.headers.callerid } : {}
       return {
         uri: `${chApi}${req.path}${query}`,
         headers: {
           'api-id': chApiId,
           'api-key': chApiKey,
-          userType: 'external'
+          usertype: 'external',
+          ...callerId
         }
       }
     },
-    onResponse: async (err, res, req, h) => {
+    onResponse: async (e, res, req, h) => {
       const payload = await wreck.read(res, { json: true })
       const response = h.response(payload)
       response.headers = res.headers
       return response
-    },
-    passThrough: true
+    }
   }
 }
 
-module.exports = [{
-  method: 'GET',
-  path: '/{path*}',
-  options: {
-    description: 'Get calls on Crown Hosting endpoints',
-    notes: 'Returns data from Crown Hosting endpoint',
-    tags: ['api']
+module.exports = [
+  {
+    method: 'GET',
+    path: '/{path*}',
+    options: {
+      description: 'Get calls on Crown Hosting endpoints',
+      notes: 'Returns data from Crown Hosting endpoint',
+      tags: ['api']
+    },
+    handler: {
+      proxy: proxyCall()
+    }
   },
-  handler: {
-    proxy: proxyCall()
+  {
+    method: 'POST',
+    path: '/{path*}',
+    options: {
+      description: 'Post calls on Crown Hosting endpoints',
+      notes: 'Returns data from Crown Hosting endpoint',
+      tags: ['api'],
+      payload: { parse: false }
+    },
+    handler: {
+      proxy: proxyCall()
+    }
   }
-},
-{
-  method: 'POST',
-  path: '/{path*}',
-  options: {
-    description: 'Post calls on Crown Hosting endpoints',
-    notes: 'Returns data from Crown Hosting endpoint',
-    tags: ['api'],
-    payload: { parse: false }
-  },
-  handler: {
-    proxy: proxyCall()
-  }
-}]
+]
