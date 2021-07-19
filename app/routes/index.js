181adfaeb8ab33df9f5488facf77a1f812aa152a
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2021-07-19 15:44:29 +0100

    Set headers required for CH connection (#6)
    * Set headers required for CH connection
    
    * Trying out some settings
    
    * turn off logging
    
    * fix req
    
    * add h to onResponse
    
    * typo
    
    * Test header forwarding
    
    * Test header passthrough
    
    * Test header passthrough
    
    * tidy up
    
    * fix
    
    * tidy up
    
    * Inject callerId rather than use passthrough
    
    * Logging headers
    
    * Remove response headers
    
    On large responses from CH the following header is set transfer-encoding: chunked
    The response then adds the content-length header
    You aren't allowed both these headers, causing wreck error on receipt
    Not copying the headers from the CH response seems the simplest way to avoid this

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,52 +1,53 @@
 const wreck = require('@hapi/wreck')
 const { chApi, chApiId, chApiKey } = require('../config')
 
-module.exports = [{
-  method: 'GET',
-  path: '/{path*}',
-  options: {
-    description: 'Get calls on Crown Hosting endpoints',
-    notes: 'Returns data from Crown Hosting endpoint',
-    tags: ['api']
-  },
-  handler: (request, h) => {
-    return h.proxy(proxyCall(request, h))
-  }
-},
-{
-  method: 'POST',
-  path: '/{path*}',
-  options: {
-    description: 'Post calls on Crown Hosting endpoints',
-    notes: 'Returns data from Crown Hosting endpoint',
-    tags: ['api'],
-    payload: { parse: false }
-  },
-  handler: (request, h) => {
-    return h.proxy(proxyCall(request, h))
-  }
-}]
-
-const proxyCall = (request, h) => {
+const proxyCall = () => {
   return {
     mapUri: (req) => {
-      const query = request.url.search ? request.url.search : ''
+      const query = req.url.search ? req.url.search : ''
+      const callerId = req.headers.callerid ? { callerid: req.headers.callerid } : {}
       return {
-        uri: `${chApi}${request.path}${query}`,
+        uri: `${chApi}${req.path}${query}`,
         headers: {
           'api-id': chApiId,
-          'api-key': chApiKey
-        },
-        passThrough: true,
-        xforward: true
+          'api-key': chApiKey,
+          usertype: 'external',
+          ...callerId
+        }
       }
     },
-    onResponse: async (err, res) => {
-      console.log(err)
+    onResponse: async (e, res, req, h) => {
       const payload = await wreck.read(res, { json: true })
       const response = h.response(payload)
-      response.headers = res.headers
       return response
     }
   }
 }
+
+module.exports = [
+  {
+    method: 'GET',
+    path: '/{path*}',
+    options: {
+      description: 'Get calls on Crown Hosting endpoints',
+      notes: 'Returns data from Crown Hosting endpoint',
+      tags: ['api']
+    },
+    handler: {
+      proxy: proxyCall()
+    }
+  },
+  {
+    method: 'POST',
+    path: '/{path*}',
+    options: {
+      description: 'Post calls on Crown Hosting endpoints',
+      notes: 'Returns data from Crown Hosting endpoint',
+      tags: ['api'],
+      payload: { parse: false }
+    },
+    handler: {
+      proxy: proxyCall()
+    }
+  }
+]
